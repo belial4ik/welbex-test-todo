9989e30882f5aa6c846c9a4990adb73e2f33694c
Belial <belyal4ik@gmail.com>
2022-02-14 00:53:15 +0300

    initial commit

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import classes from './main.module.css'
+import logo from '../../assets/Todo-Logo.png'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className={classes.desriptionPanel}>
+      <img src={logo} alt="img" width={300}/>
+      <div>
+        <h2>This is my to-do list app, at the top of the page there is a transition between pages, you can go to the to-do list, where you can add, edit and delete the to-do list</h2>
+      </div>
+    </div>
   )
 }
 
--- src/pages/todo-list/todo-list.js
+++ src/pages/todo-list/todo-list.js
@@ -1,24 +1,82 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
+import classes from './todo-list.module.css'
 
 const TodoList = () => {
+  const [ value, setValue  ] = useState('')
   const [todos, setTodos] = useState([])
+  const [ visible, setVisible ] = useState(9)
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(res => res.json())
-      .then(data => setTodos(data))
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+      .then(res => setTodos(res.data))
+    
   }, [])
+
+  const loadMore = () => {
+    setVisible(visible + 9)
+  }
+
+  const handleChange = e => {
+    setValue(e.target.value)
+  }
+
+  const deleteItem = (id) => {
+    const newItem = [...todos]
+    const res = newItem.filter((item) => {
+      return item.id !== id
+    })
+    setTodos(res)
+  }
+
+  const switchHandler = id => {
+    const newItem = [...todos]
+    newItem.filter(item => {
+      if(item.id === id) {
+        item.completed = !item.completed
+      }
+    })
+    setTodos(newItem)
+  }
+
+  const addButton = () => {
+    const item = {
+      userId: 11,
+      id: 201 + Math.floor(Math.random() * 1000),
+      title: value,
+      completed: false
+    }
+    const newItem = [...todos, item]
+    setTodos(newItem)
+    setValue('')
+  }
+
   return (
     <>
+      <div className={classes.todoList}>
+        <div>
+          <input value={value} onChange={handleChange} />
+          <button className={classes.addNewItemButton} onClick={addButton}>add</button>
+        </div>
       {
-        todos.map(({id,title, completed}) => (
+        todos
+          .slice(0, visible)
+          .map(({ id, title, completed }) => (
           <div key={id}>
-            <p>{id}</p>
-            <p>{title}</p>
-            <p>{completed ? 'сделано' : "не сделано"}</p>
+            <div className={classes.card}>
+              <h3 style={{cursor: 'pointer'}} onClick={() => switchHandler(id)} className={completed ? classes.completed : ''}>{title}</h3>
+              <button onClick={() => deleteItem(id)}>delete</button>
+            </div>
           </div>
         ))
       }
+      </div>
+      <div style={{textAlign: 'center'}}>
+        {
+          visible < todos.length && <button className={classes.showMore} onClick={loadMore}>show more</button>
+        }
+      </div>
     </>
+    
   )
 }
 
